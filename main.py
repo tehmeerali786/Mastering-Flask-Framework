85e31802d08a4db71baf1e7446ec6f2c3b18d533
unknown <>
2021-03-09 10:52:33 -0800

    second commit

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-hello = "hello"
-print(hello)
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "<h1>Hello World</h1>"
+
+
+if __name__ == "__main__":
+    app.run()
