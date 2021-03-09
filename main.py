b31dbe68cc34b5aaaa80d7bb918b60d051081a71
unknown <>
2021-03-09 11:01:30 -0800

    Display good morning because its nice

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/username')
 def home():
-    return "<h1>Hello World</h1>"
+    return "<h1>Good Morning</h1>" % username
 
 
 if __name__ == "__main__":
